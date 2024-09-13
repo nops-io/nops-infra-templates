2fafde70c05dd78abebb4417e8212ce545faa264
Alonso Parasxidis <alonso.parasxidis@gmail.com>
2024-09-12 22:28:36 -0700

    Update main.tf

--- karpenter-lab/main.tf
+++ karpenter-lab/main.tf
@@ -18,6 +18,20 @@ provider "kubernetes" {
   }
 }
 
+provider "kubectl" {
+  apply_retry_count      = 5
+  host                   = var.cluster_endpoint
+  cluster_ca_certificate = base64decode(var.cluster_certificate_authority_data)
+  load_config_file       = false
+
+  exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    command     = "aws"
+    # This requires the awscli to be installed locally where Terraform is executed
+    args = ["eks", "get-token", "--cluster-name", var.cluster_name]
+  }
+}
+
 provider "helm" {
   kubernetes {
     host                   = var.cluster_endpoint
@@ -158,48 +172,6 @@ resource "kubectl_manifest" "karpenter_node_pool" {
       disruption:
         consolidationPolicy: WhenEmpty
         consolidateAfter: 30s
-  YAML
-
-  depends_on = [
-    module.eks_blueprints_addons_karpenter
-  ]
-}
-
-# Example deployment using the [pause image](https://www.ianlewis.org/en/almighty-pause-container)
-# and starts with zero replicas
-resource "kubectl_manifest" "karpenter_example_deployment" {
-  yaml_body = <<-YAML
-    apiVersion: apps/v1
-    kind: Deployment
-    metadata:
-      name: inflate
-    spec:
-      replicas: 0
-      selector:
-        matchLabels:
-          app: inflate
-      template:
-        metadata:
-          labels:
-            app: inflate
-        spec:
-          terminationGracePeriodSeconds: 0
-          containers:
-            - name: inflate
-              image: public.ecr.aws/eks-distro/kubernetes/pause:3.7
-              resources:
-                requests:
-                  cpu: 1
-        topologySpreadConstraints:
-        - maxSkew: 1
-          topologyKey: topology.kubernetes.io/zone
-          whenUnsatisfiable: ScheduleAnyway
-          labelSelector:
-            matchExpressions:
-            - key: app
-              operator: In
-              values:
-              - "inflate" 
   YAML
 
   depends_on = [
